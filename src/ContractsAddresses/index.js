17d6857c1efbf21193337412d57078e203ef28b6
abbas nosrati <abbas.nosrati68@gmail.com>
2022-07-20 17:56:43 +0430

    change contract address

--- src/ContractsAddresses/index.js
+++ src/ContractsAddresses/index.js
@@ -1,4 +1,4 @@
-export  const UNBCNFTContractAddress = '0x4b74e0B90d12f2DbFfF25C830b44B2093F2F9d32'
-export  const minterContractAddress = '0x9c40A534C223A8D7330E5b1b9Ad3a0A424f4DB75'
-export  const settingContractAddress = '0x12819aF5cE88604B7A6Ac6dbE16A3Bca5e79d296'
+export  const UNBCNFTContractAddress = '0x929Ccf84BE21B3A7a71f3D8DE062394E0FA74541'
+export  const minterContractAddress = '0xDbD47473D4F9a9b448162254a6BFE7d4DcbB2378'
+export  const settingContractAddress = '0x63123BD52916f820cB22D9f6638A12304Ae729cC'
 export  const utopiaFactoryContractAddress   = '0xBFC07f06D05FfeF1735dB08ef43c811bcAF61bbA'
