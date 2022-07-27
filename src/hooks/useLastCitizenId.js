bad4b08820f32b9fb9df77ea56bd2ce161931e7f
abbas nosrati <abbas.nosrati68@gmail.com>
2022-07-27 14:31:37 +0430

    edit setbright id

---
+++ src/hooks/useLastCitizenId.js
@@ -0,0 +1,17 @@
+import { unbcNFTAbi } from '../ABI/ABI'
+import { getContract } from '../utils/contractHelpers'
+import useWeb3 from './useWeb3'
+import { UNBCNFTContractAddress } from '../ContractsAddresses'
+
+const useLastCitizenId = () => {
+    const web3 = useWeb3()
+    const getLastID = async (account, ) => {
+        const contract = await getContract(unbcNFTAbi, UNBCNFTContractAddress, web3)
+        const citizenID = await contract.methods.getCitizenID(account).call()
+        return citizenID
+    }
+
+    return getLastID
+}
+
+export default useLastCitizenId
