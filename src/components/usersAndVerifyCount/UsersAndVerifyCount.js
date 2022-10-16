64cacbb6fb9e3f0c7ccbee7e655e623b2ebe7dfa
abbas nosrati <abbas.nosrati68@gmail.com>
2022-10-16 21:45:55 +0330

    change title

--- src/components/usersAndVerifyCount/UsersAndVerifyCount.js
+++ src/components/usersAndVerifyCount/UsersAndVerifyCount.js
@@ -21,13 +21,13 @@ const UsersAndVerifyCount = () => {
       <>
         <div style={{display:'flex',flexDirection:'column',alignItems:'center',width:'100%', marginTop:'20px', color:'#814f8c', borderTop:'2px solid #fff', textAlign:'left'}}>
           <div style={{display:'flex', columnGap: '10px', marginTop:'10px', alignItems:'left', justifyContent:'left',minWidth:'120px'}}>
-              Total users:
+              Utopia42 Citizens:
               <div>
                 {totalSupply}
               </div>
           </div>
           <div style={{display:'flex', columnGap: '10px',alignItems:'center', justifyContent:'left',minWidth:'120px'}}>
-            Verified users:
+            Verified Citizens:
             <div>
               {citizenIDVerifications}
             </div>
